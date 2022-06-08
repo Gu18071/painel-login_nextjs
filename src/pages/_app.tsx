fbe2ccd49002c2dc5f6223827af9b30149cde030
GU18071 <gus.cianci18@hotmail.com>
2022-06-08 19:04:23 -0300

    add nwe features

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { AutenticacaoProvider } from '../contexts/AutenticacaoContext';
+import '../../src/styles/styles.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+        <AutenticacaoProvider>
+            <Component {...pageProps} />f
+        </AutenticacaoProvider>
+
+  )
 }
 
 export default MyApp
--- src/pages/usuario/index.tsx
+++ src/pages/usuario/index.tsx
@@ -1,17 +1,166 @@
+import  Head  from 'next/head';
 import { useRouter } from 'next/router'
+import { parseCookies } from "nookies";
+import { GetServerSideProps } from "next";
+import { Menu } from '../../components/Menu';
+import { validaPermissao } from "../../services/validaPermissao";
+import { useEffect, useState } from 'react';
+import api from '../../services/request';
 
-export default function Usuario() {
+
+
+
+
+
+interface interfaceProps{
+    token?: string;
+}
+
+
+interface interfaceUsuario{
+    bairro?: string,
+    cpf?: string,
+    email: string,
+    endereco?: string,
+    id: number,
+    nome: string,
+    numero?: string,
+    telefone: string,
+    tipo: string,
+}
+
+
+
+
+
+
+export default function Usuario(props: interfaceProps) {
+
+    const [usuario, setUsuario] = useState<Array<interfaceUsuario>>([]);
+    const cookies = parseCookies();
+
+
+
+
+
+    useEffect(()=>{
+        const config = {
+            headers: {
+                Authorization: `Bearer ${cookies['painel-token']}`,
+            },
+        };
+
+        api.get(`/usuarios`,config).then(response=>{
+
+            console.log(response.data);
+            setUsuario(response.data);
+
+        }).catch((error)=>{
+
+        })
+    },[]);
 
     const router = useRouter();
 
     return (
         <>
-            <h1>Usuario</h1>
-            <button onClick={() => {
+            <Head>
+                <title>Usuario</title>
+            </Head>
 
-                router.push('/dashboard/paraguai');
+            <Menu active="usuario" token={props.token}>
+                <>
+                <div className={"d-flex justify-content-between flex-wrap  flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom"}>
+                        <h2>Usuario</h2>
+                        <div className={"btn-toolbar mb-2 mb-0 "}>
+                            <button type="button" className="btn btn-success"
+                            onClick={()=>{router.push('/usuario/criar')}}
+                            >
+                                Adicionar
+                            </button>
+                        </div>
+                    </div>
 
-            }}>Ir para o dashboard</button>
+
+                       <table className="table table-striped">
+                           <thead>
+                               <tr>
+                                   <th>ID</th>
+                                   <th>Nome</th>
+                                   <th>Email</th>
+                                   <th>Açoes</th>
+
+                               </tr>
+                           </thead>
+                           <tbody>
+                           {
+                               usuario.map((user, index) =>{
+                                   return(
+                                    <tr key={user.id}>
+                                        <td>ID: {user.id}</td>
+                                        <td>Nome: {user.nome}</td>
+                                        <td>Email: {user.email}</td>
+                                        <td>
+                                            <button className="btn btn-primary " onClick={() =>{
+                                                router.push('/usuario/' + user.id)
+                                            }} style={{marginLeft:5}}>
+                                                Editar
+                                             </button>
+
+                                            <button className="btn btn-danger" onClick={() =>{
+
+                                            }}>
+                                                Deletar
+
+                                            </button>
+                                        </td>
+                                    </tr>
+                                   )
+                                })
+                           }
+
+
+                           </tbody>
+
+                       </table>
+
+
+
+                </>
+            </Menu>
         </>
     )
 }
+
+export const getServerSideProps: GetServerSideProps = async (contexto)=>{
+
+    const {'painel-token': token} = parseCookies(contexto);
+    console.log(token);
+
+    if (!token) {
+            return{
+                redirect:{
+                    destination: '/login',
+                    permanent: false
+                }
+            }
+    }
+
+    const temPermissaoPage = validaPermissao(token, ['admin']);
+
+    if (!temPermissaoPage) {
+        return{
+            redirect:{
+                destination: '/dashboard',
+                permanent: false
+            }
+        }
+    }
+
+    return {
+        props : {
+            token
+        }
+    }
+}
+
